7aa2a5c6995783b05f0506b5fe9cf10cd2ab8073
Tanner Helton <tanner@tannerhelton.com>
2023-08-21 14:44:02 -0500

    Update Schedule.js

--- frontend/src/screens/Schedule.js
+++ frontend/src/screens/Schedule.js
@@ -3,11 +3,7 @@ import { View } from "react-native";
 import LaundryCardContainer from "../components/LaundryCardContainer";
 
 function LaundryScreen({ navigation }) {
-  return (
-    <View>
-      <LaundryCardContainer />
-    </View>
-  );
+  return <LaundryCardContainer />;
 }
 
 export default LaundryScreen;
