55cab161ab3b9171136278ba13dc50213ee890af
Tanner Helton <tanner@tannerhelton.com>
2023-09-18 13:31:49 -0500

    add firebase

--- frontend/src/screens/LoginPage.js
+++ frontend/src/screens/LoginPage.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField, Typography } from "@mui/material";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const auth = getAuth();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      // Navigate to Chatbot page or wherever you want
+    } catch (error) {
+      console.error("Login Failed:", error);
+    }
+  };
+
   return (
     <div>
       <Typography variant="h1">Login</Typography>
-      <form>
-        <TextField label="Username" variant="outlined" />
-        <TextField label="Password" variant="outlined" type="password" />
+      <form onSubmit={handleLogin}>
+        <TextField
+          label="Email"
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <Button variant="contained" color="primary" type="submit">
           Login
         </Button>
