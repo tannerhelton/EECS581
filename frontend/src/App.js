d1ed7d50f1dd8fa371376a8cb9397abfed0d474f
Tanner Helton <tanner@tannerhelton.com>
2023-09-24 10:33:21 -0500

    added firebase config back

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,13 @@ import ChatbotPage from "./screens/ChatbotPage";
 
 // Firebase configuration object
 const firebaseConfig = {
-  // ... (your Firebase config here)
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase app
