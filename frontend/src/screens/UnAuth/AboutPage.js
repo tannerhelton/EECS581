57452c8ea4baf650c4df803402b74db61374394b
Tanner Helton <tanner@tannerhelton.com>
2023-10-02 12:25:39 -0500

    added about page

---
+++ frontend/src/screens/UnAuth/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Typography variant="h1">About Health Horizon AI</Typography>
+      <Typography variant="body1">Early detection, better health.</Typography>
+    </div>
+  );
+};
+
+export default AboutPage;
