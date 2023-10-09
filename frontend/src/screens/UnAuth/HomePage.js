9062e8ece7064cc886afc16b87a6b1e67753ea4d
Tanner Helton <tanner@tannerhelton.com>
2023-10-09 12:16:11 -0500

    removed env var for PUBLIC_URL

--- frontend/src/screens/UnAuth/HomePage.js
+++ frontend/src/screens/UnAuth/HomePage.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Typography, Paper, Container } from "@mui/material";
 import { styled } from "@mui/system";
 
-const logoUrl = process.env.PUBLIC_URL + "/HH_logo.png";
+const logoUrl = "/HH_logo.png";
 
 const StyledContainer = styled(Container)({
   padding: (theme) => theme.spacing(4),
   marginTop: (theme) => theme.spacing(4),
   textAlign: "center",
-  backgroundColor: 'transparent', // Making the Paper's background transparent
+  backgroundColor: "transparent", // Making the Paper's background transparent
 });
 
 const LogoImage = styled("img")({
@@ -18,9 +18,13 @@ const LogoImage = styled("img")({
 
 const HomePage = () => {
   return (
-    <StyledContainer component={Paper} elevation={0}> {/* Removed shadow with elevation={0} */}
+    <StyledContainer component={Paper} elevation={0}>
+      {" "}
+      {/* Removed shadow with elevation={0} */}
       <LogoImage src={logoUrl} alt="Health Horizon AI Logo" />
-      <Typography variant="h2" color="primary"> {/* Changed to h2 variant for larger text and specified color */}
+      <Typography variant="h2" color="primary">
+        {" "}
+        {/* Changed to h2 variant for larger text and specified color */}
         Early detection, better health.
       </Typography>
     </StyledContainer>
