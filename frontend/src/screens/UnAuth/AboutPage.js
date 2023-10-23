85ebfab5b31a131d5e8c6d1cbb698ad38f6db96e
Tanner Helton <tanner@tannerhelton.com>
2023-10-23 11:28:52 -0500

    Resolve duplicate LogoImage

--- frontend/src/screens/UnAuth/AboutPage.js
+++ frontend/src/screens/UnAuth/AboutPage.js
@@ -2,84 +2,89 @@ import React from "react";
 import { Typography, Grid, Paper, Container, Avatar } from "@mui/material";
 import { styled } from "@mui/system";
 
-
 const logoUrl = process.env.PUBLIC_URL + "/HH_logo.png";
 const heartDiseaseUrl = process.env.PUBLIC_URL + "/logo512.png";
 const cancerUrl = process.env.PUBLIC_URL + "/logo512.png";
 const diabetesUrl = process.env.PUBLIC_URL + "/logo512.png";
 
-
 const LogoImage = styled("img")({
   maxWidth: "30%",
   marginBottom: (theme) => theme.spacing(3),
 });
 
-const InnerWrapper = styled('div')({
-  paddingBottom: '200px',
+const InnerWrapper = styled("div")({
+  paddingBottom: "200px",
 });
 const StyledContainer = styled(Container)({
   padding: (theme) => theme.spacing(4),
-  marginTop: '25vh',
-  marginBottom: '100px',
+  marginTop: "25vh",
+  marginBottom: "100px",
   textAlign: "center",
-  backgroundColor: 'transparent',
+  backgroundColor: "transparent",
 });
 
 const BlocksContainer = styled(Container)({
-  padding: '20px',
-  marginTop: '20vh',
+  padding: "20px",
+  marginTop: "20vh",
   textAlign: "center",
-  backgroundColor: 'transparent',
-  width: '50%',
-  borderRadius: '20px',
-  backgroundColor: '#2c3e50',
-  boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
-});
-
-const LogoImage = styled("img")({
-  maxWidth: "30%",
-  marginBottom: (theme) => theme.spacing(3),
+  backgroundColor: "transparent",
+  width: "50%",
+  borderRadius: "20px",
+  backgroundColor: "#2c3e50",
+  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
 });
 
 const FeatureBlock = ({ icon, title, description }) => (
   <Grid item xs={12} sm={4}>
-    <Avatar variant="rounded" style={{ margin: '0 auto', marginBottom: '20px', backgroundColor: 'transparent'}}>
+    <Avatar
+      variant="rounded"
+      style={{
+        margin: "0 auto",
+        marginBottom: "20px",
+        backgroundColor: "transparent",
+      }}
+    >
       {icon}
     </Avatar>
-    <Typography variant="h5" color="primary">{title}</Typography>
-    <Typography variant="body1" style={{ color: '#FFFFFF' }}>{description}</Typography>
+    <Typography variant="h5" color="primary">
+      {title}
+    </Typography>
+    <Typography variant="body1" style={{ color: "#FFFFFF" }}>
+      {description}
+    </Typography>
   </Grid>
 );
-
 
 const AboutPage = () => {
   return (
     <StyledContainer component={Paper} elevation={0}>
-      <Typography variant="h2" color="primary">About Health Horizon AI</Typography>
-      <Typography variant="h6" color="primary" style={{ marginTop: '20px' }}>
-        At Health Horizon AI, we are dedicated to leveraging the power of
-        machine learning to advance healthcare. Our mission is to pioneer
-        early detection technologies for various medical conditions, including
-       heart disease, cancer, diabetes, and more.
+      <Typography variant="h2" color="primary">
+        About Health Horizon AI
       </Typography>
-      <div style={{ height: '300px', overflowY: 'scroll' }}></div>
-        <Grid container spacing={6}>
-          <FeatureBlock 
-            src={logoUrl}
-            title="Heart Disease" 
-            description="We leverage linear regression algorithms to detect early heart disease in patients." 
-          />
-          <FeatureBlock 
-            src={logoUrl} 
-            title="Cancer" 
-            description="Our neural networks are meticulously trained to accurately detect cancer symptoms." 
-          />
-          <FeatureBlock 
-            src={logoUrl} 
-            title="Diabetes" 
-            description="Our trained data models and sophisticated AI algorithms allow us to create catered medical plans for diabetic patients.." 
-          />
-        </Grid>
+      <Typography variant="h6" color="primary" style={{ marginTop: "20px" }}>
+        At Health Horizon AI, we are dedicated to leveraging the power of
+        machine learning to advance healthcare. Our mission is to pioneer early
+        detection technologies for various medical conditions, including heart
+        disease, cancer, diabetes, and more.
+      </Typography>
+      <div style={{ height: "300px", overflowY: "scroll" }}></div>
+      <Grid container spacing={6}>
+        <FeatureBlock
+          src={logoUrl}
+          title="Heart Disease"
+          description="We leverage linear regression algorithms to detect early heart disease in patients."
+        />
+        <FeatureBlock
+          src={logoUrl}
+          title="Cancer"
+          description="Our neural networks are meticulously trained to accurately detect cancer symptoms."
+        />
+        <FeatureBlock
+          src={logoUrl}
+          title="Diabetes"
+          description="Our trained data models and sophisticated AI algorithms allow us to create catered medical plans for diabetic patients.."
+        />
+      </Grid>
     </StyledContainer>
   );
 };
