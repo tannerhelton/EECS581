27ef3f626ea96cb3f823a206d9a6f2bb47da761d
Adam Jolles <adamjolles8@gmail.com>
2023-10-22 19:55:35 -0500

    Update AuthHomePage

--- frontend/src/screens/Auth/AuthHomePage.js
+++ frontend/src/screens/Auth/AuthHomePage.js
@@ -1,19 +1,41 @@
-// Importing the required components and modules
 import React from "react";
-import { Typography } from "@mui/material";
+import { Typography, Grid, Paper, Container, Avatar } from "@mui/material";
+import { styled } from "@mui/system";
 
-// HomePage component definition
+const logoUrl = process.env.PUBLIC_URL + "/HH_logo.png";
+const neuralUrl = process.env.PUBLIC_URL + "/neural.png";
+
+const StyledContainer = styled(Container)({
+  padding: (theme) => theme.spacing(4),
+  marginTop: (theme) => theme.spacing(4),
+  textAlign: "center",
+  backgroundColor: 'transparent',
+});
+
+const LogoImage = styled("img")({
+  maxWidth: "30%",
+  marginBottom: (theme) => theme.spacing(3),
+});
+
+const FeatureBlock = ({ icon, title, description }) => (
+  <Grid item xs={12} sm={4}>
+    <Avatar variant="rounded" style={{ margin: '0 auto', marginBottom: '20px', backgroundColor: 'transparent'}}>
+      {icon}
+    </Avatar>
+    <Typography variant="h5" color="primary">{title}</Typography>
+    <Typography variant="body1" style={{ color: '#FFFFFF' }}>{description}</Typography>
+  </Grid>
+);
+
 const HomePage = () => {
   return (
-    // Main div container
-    <div>
-      {/* Display a welcome message using Typography with "h1" variant */}
-      <Typography variant="h1">
-        Welcome to Health Horizon AI, you have been authenticated
+    <StyledContainer component={Paper} elevation={0}>
+      <LogoImage src={logoUrl} alt="Health Horizon AI Logo" />
+      <Typography variant="h2" color="primary"> {/* Changed to h2 variant for larger text and specified color */}
+        Early detection, better health.
       </Typography>
-    </div>
+    </StyledContainer>
   );
 };
 
-// Export the HomePage component for use in other parts of the app
 export default HomePage;
