1ac711b1513dcb6d40d4183ec06419e8686cc9c7
Tanner Helton <tanner@tannerhelton.com>
2023-10-23 12:36:52 -0500

    optimized chatGPT integration

--- backend/OpenAI/app.py
+++ backend/OpenAI/app.py
@@ -1,32 +1,46 @@
 import os
 import openai
-
 from dotenv import load_dotenv
 
-load_dotenv()
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
+def initialize_api_key():
+    load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    if not openai.api_key:
+        raise ValueError("OPENAI_API_KEY not found in environment!")
 
 def get_basic_info(name, sex, weight, height, age, activity_level, medical_diagnosis):
-    message = f"My name is {name}. I am a {sex} I weigh {weight} pounds and I am {height} inches tall. I am {age} years old and my activity level is {activity_level}. I have been diagnosed with {medical_diagnosis}. Can you provide me with some basic information?"
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        max_tokens=100,
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": message},
-        ]
+    message = (
+        f"My name is {name}. I am a {sex.capitalize()}. I weigh {weight} pounds and "
+        f"I am {height} inches tall. I am {age} years old and my activity level is "
+        f"{activity_level}. I have been diagnosed with {medical_diagnosis.capitalize()}. "
+        "Can you provide me with some basic information?"
     )
-    return response['choices'][0]['message']['content']
 
-name = "John"
-sex = "male"
-weight = 180
-height = 72
-age = 35
-activity_level = "moderate"
-medical_diagnosis = "diabetes"
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            max_tokens=100,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": message},
+            ]
+        )
+        return response['choices'][0]['message']['content']
+    except openai.error.OpenAIError as error:
+        return f"Error: {error}"
 
-basic_info = get_basic_info(name, sex, weight, height, age, activity_level, medical_diagnosis)
+def main():
+    name = "John"
+    sex = "male"
+    weight = 180
+    height = 72
+    age = 35
+    activity_level = "moderate"
+    medical_diagnosis = "diabetes"
+    
+    basic_info = get_basic_info(name, sex, weight, height, age, activity_level, medical_diagnosis)
+    print(basic_info)
 
-print(basic_info)
+if __name__ == "__main__":
+    initialize_api_key()
+    main()
