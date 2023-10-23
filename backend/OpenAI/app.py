a53e183ffaa532c5dc15b0a758f2463d98e1f8b4
Tanner Helton <tanner@tannerhelton.com>
2023-10-23 11:36:24 -0500

    Use ChatCompletions instead of davinci

--- backend/OpenAI/app.py
+++ backend/OpenAI/app.py
@@ -7,23 +7,26 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def get_basic_info(name, weight, height, age, activity_level, medical_diagnosis):
-    message = f"My name is {name}. I weigh {weight} pounds and I am {height} inches tall. I am {age} years old and my activity level is {activity_level}. I have been diagnosed with {medical_diagnosis}. Can you provide me with some basic information?"
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=message,
-        temperature=0.8,
-        max_tokens=1024
+def get_basic_info(name, sex, weight, height, age, activity_level, medical_diagnosis):
+    message = f"My name is {name}. I am a {sex} I weigh {weight} pounds and I am {height} inches tall. I am {age} years old and my activity level is {activity_level}. I have been diagnosed with {medical_diagnosis}. Can you provide me with some basic information?"
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        max_tokens=100,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message},
+        ]
     )
-    return response.choices[0].text.strip()
+    return response['choices'][0]['message']['content']
 
 name = "John"
+sex = "male"
 weight = 180
 height = 72
 age = 35
 activity_level = "moderate"
 medical_diagnosis = "diabetes"
 
-basic_info = get_basic_info(name, weight, height, age, activity_level, medical_diagnosis)
+basic_info = get_basic_info(name, sex, weight, height, age, activity_level, medical_diagnosis)
 
 print(basic_info)
