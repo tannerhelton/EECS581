74c8ad642a77599f8c859de20f5011ca34ab59fd
tomgans <tomgans14@gmail.com>
2023-11-02 13:16:33 -0500

    MLresults

--- frontend/src/screens/Auth/MLResults.js
+++ frontend/src/screens/Auth/MLResults.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./css/MLResults.css"; // Create a CSS file for styling
 import { Link } from "react-router-dom";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 function MLResults() {
-	// Replace this with your actual Matplotlib results data
-	const matplotlibResults = "You have cancer";
+	const [matplotlibResults, setMatplotlibResults] = useState([]);
+
+	// Make an API request to your Python server to fetch the PNG images
+	useEffect(() => {
+		fetch("/api/matplotlib-results") // Replace with your API endpoint
+			.then((response) => response.json())
+			.then((data) => {
+				setMatplotlibResults(data);
+			})
+			.catch((error) => {
+				console.error("Error fetching data:", error);
+			});
+	}, []);
 
 	return (
 		<div className="ml-results-card">
 			<h2>Matplotlib Results</h2>
-			<div className="ml-results-content">{matplotlibResults}</div>
+			<div className="ml-results-content">
+				{matplotlibResults.map((result, index) => (
+					<img key={index} src={result} alt={`Matplotlib Result ${index}`} />
+				))}
+			</div>
 			<Link to="/profile" className="back-to-profile">
 				<ArrowBackIosNewIcon />
 			</Link>
