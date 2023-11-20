39326986d8089ee478cc91b3251232177adc69bf
tomgans <tomgans14@gmail.com>
2023-11-19 21:36:09 -0600

    working poc, just need chris py

--- frontend/src/screens/Auth/MLResults.js
+++ frontend/src/screens/Auth/MLResults.js
@@ -1,17 +1,57 @@
-import React from "react";
-import "./css/MLResults.css"; // Create a CSS file for styling
+import React, { useState } from "react";
+import "./css/MLResults.css";
 import { Link } from "react-router-dom";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 function MLResults() {
-	// Replace this with your actual Matplotlib results data
-	const matplotlibResults = "You have cancer";
+	const [imageData, setImageData] = useState([]);
+	const [textResult, setTextResult] = useState("");
+	const [buttonClicked, setButtonClicked] = useState(false);
+
+	const generateImage = () => {
+		// Fetch image data
+		fetch("/api/generate-plot")
+			.then((response) => response.text())
+			.then((dataUrl) => {
+				setImageData((prevImageData) => [...prevImageData, dataUrl]);
+			})
+			.catch((error) => {
+				console.error("Error generating image:", error);
+			});
+
+		// Fetch text result
+		fetch("/api/text-results")
+			.then((response) => response.text())
+			.then((result) => {
+				setTextResult(result);
+			})
+			.catch((error) => {
+				console.error("Error fetching text results:", error);
+			});
+
+		// Set the buttonClicked state to true
+		setButtonClicked(true);
+	};
 
 	return (
 		<div className="ml-results-card">
-			<h2>Matplotlib Results</h2>
-			<div className="ml-results-content">{matplotlibResults}</div>
-			<Link to="/profile" className="back-to-profile">
+			<h2>Your Results</h2>
+			{buttonClicked ? (
+				<p></p>
+			) : (
+				<button type="button" onClick={generateImage}>
+					Generate
+				</button>
+			)}
+			<div className="ml-results-content">
+				{textResult && <p>{textResult}</p>}
+				{imageData.map((image, index) => (
+					<div className="ml-results-image-card" key={index}>
+						<img src={image} alt={`Generated Image ${index + 1}`} />
+					</div>
+				))}
+			</div>
+			<Link to="/" className="back-to-profile">
 				<ArrowBackIosNewIcon />
 			</Link>
 		</div>
