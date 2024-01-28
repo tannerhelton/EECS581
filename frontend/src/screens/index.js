0028820d726d8ed98514fa13810c2c065657719b
ArmoredFuzzball <armoredredfox@gmail.com>
2024-01-28 01:42:42 +0000

    Add file upload page and upload logic

--- frontend/src/screens/index.js
+++ frontend/src/screens/index.js
@@ -2,6 +2,7 @@ export { default as HomePage } from "./UnAuth/HomePage";
 export { default as AuthHomePage } from "./Auth/AuthHomePage";
 export { default as LoginPage } from "./UnAuth/LoginPage";
 export { default as ChatbotPage } from "./Auth/ChatbotPage";
+export { default as UploadPage } from "./Auth/UploadPage";
 export { default as ProfilePage } from "./Auth/ProfilePage";
 export { default as AboutPage } from "./UnAuth/AboutPage";
 export { default as MLResults } from "./Auth/MLResults";
